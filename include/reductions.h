3b9117f5a809b6ae15049a5a7df704dbb20477bd
ernestine-grossmann <e.grossmann@stud.uni-heidelberg.de>
2024-09-14 12:50:55 +0200

    add extended single edge

--- include/reductions.h
+++ include/reductions.h
@@ -41,5 +41,8 @@ int reduction_domination_csr(reduction_data *R, int N, const int *V, const int *
 int reduction_single_edge_csr(reduction_data *R, int N, const int *V, const int *E,
                          const long long *W, const int *A, int u, int* nRed, int* reducable);
 
+int reduction_extended_single_edge_csr(reduction_data *R, int N, const int *V, const int *E,
+                         const long long *W, const int *A, int u, int* nRed, int* reducable);
+
 int reduction_unconfined_csr(reduction_data *R, int N, const int *V, const int *E,
                              const long long *W, const int *A, int u, int* nRed, int* reducable);
